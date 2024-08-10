3c21484ffdff63456c838fac8ea17899c746eda4
Max7774 <mega_ymbetov@mail.ru>
2024-08-10 22:25:31 +0300

    [Styles]

--- src/App/UI/Card/Card.tsx
+++ src/App/UI/Card/Card.tsx
@@ -5,12 +5,12 @@ interface ICardProps {
 
 const Card = ({ text, Icon }: ICardProps) => {
   return (
-    <div className="card relative bg-gray mx-3 my-7 rounded-[30px] px-4 py-5 overflow-hidden">
+    <div className="card relative bg-gray mx-3 my-5 rounded-[25px] px-4 py-5 overflow-hidden">
       <div className="flex flex-row gap-7 justify-between relative z-10">
         <div>{Icon}</div>
         <div className="text-mainText text-left">{text}</div>
       </div>
-      <div className="glare-effect absolute inset-0 z-0 rounded-[30px]"></div>
+      <div className="glass-effect absolute inset-0 z-0 rounded-[25px]"></div>
     </div>
   );
 };
