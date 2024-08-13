25b5151b9cd24512f552c4b7023d306c91c407e6
Max7774 <mega_ymbetov@mail.ru>
2024-08-13 19:49:57 +0300

    [Api]

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default () => {
-  const env = loadEnv("all", process.cwd());
   return defineConfig({
     plugins: [react()],
     build: {
@@ -13,8 +12,9 @@ export default () => {
       port: 3000,
       proxy: {
         "/api/v1": {
-          target: env.VITE_BASE_URL,
+          target: "https://app.leadteh.ru",
           changeOrigin: true,
+          secure: false, // Включите, если сервер https с самоподписанным сертификатом
           rewrite: (path) => path.replace(/^\/api\/v1/, ""),
         },
       },
